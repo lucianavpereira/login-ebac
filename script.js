22d2fd05ed91868173400bf8cdde050e42796629
Luciana V Pereira <115096459+lucianavpereira@users.noreply.github.com>
2023-05-04 18:23:28 -0300

    projeto em construção da jornada Ebac

---
+++ script.js
@@ -0,0 +1,10 @@
+function logar(){
+    var login = document.getElementById('login').value
+    var senha = document.getElementById('senha').value
+
+    if (login == 'admin' && senha == 'admin') {
+        alert('sucesso')
+    } else {
+        alert('Acesso negado.')
+    }
+}
