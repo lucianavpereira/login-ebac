ff3de10f3b02ccdc02c2dfbe6612c8b04777a446
Luciana V Pereira <115096459+lucianavpereira@users.noreply.github.com>
2023-05-08 20:27:52 -0300

    reorganizar código

--- script.js
+++ script.js
@@ -1,21 +1,62 @@
-function logar(){
+// function logar(){
+//     var login = document.getElementById('login').value
+//     var senha = document.getElementById('senha').value
+
+//     if (login == 'admin' && senha == 'admin') {
+//         // alert('sucesso')
+//         // ir para página de cadastro
+//         var url = 'cadastro.html'
+//         var btn = document.querySelector('#botao')
+
+//         function openInNewTab(url) {
+//             var win = window.open(url, '_blank') 
+//             win.focus()
+//         }
+//         btn.addEventListener('click', () => {
+//             openInNewTab(url)
+//         })
+
+
+//     } else {
+//         alert('Acesso negado.')
+//     }
+// }
+
+
+var url = 'cadastro.html'
+var btn = document.querySelector('#botao')
+
+//Com função nomeada----------------
+// function executa(event){
+//     event.preventDefault()
+//     logar(url)
+// }
+
+// btn.addEventListener('click', executa)
+
+//Com função anônima----------------
+// btn.addEventListener('click', function (event){
+//     event.preventDefault()
+//     logar(url)
+// })
+
+//Com arrow function-----------------------
+btn.addEventListener('click', (evento) => {
+    evento.preventDefault()
+    logar(url)
+})
+
+function logar(url){
     var login = document.getElementById('login').value
     var senha = document.getElementById('senha').value
 
     if (login == 'admin' && senha == 'admin') {
         // alert('sucesso')
         // ir para página de cadastro
-        var url = 'cadastro.html'
-        var btn = document.querySelector('#botao')
-
-        function openInNewTab(url) {
-            var win = window.open(url, '_blank') 
-            win.focus()
-        }
-        btn.addEventListener('click', () => {
-            openInNewTab(url)
-        })
-
+ 
+        window.open(url,'_self') 
+        // var win = window.open(url,'_blank') 
+        // win.focus()
 
     } else {
         alert('Acesso negado.')
