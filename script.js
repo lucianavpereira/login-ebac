56fffc6419c7c749ce177bc03100165e163eefeb
Luciana V Pereira <115096459+lucianavpereira@users.noreply.github.com>
2023-05-08 13:32:17 -0300

    teste abrir página cadastro ok

--- script.js
+++ script.js
@@ -5,7 +5,16 @@ function logar(){
     if (login == 'admin' && senha == 'admin') {
         // alert('sucesso')
         // ir para página de cadastro
-        
+        var url = 'cadastro.html'
+        var btn = document.querySelector('#botao')
+
+        function openInNewTab(url) {
+            var win = window.open(url, '_blank') 
+            win.focus()
+        }
+        btn.addEventListener('click', () => {
+            openInNewTab(url)
+        })
 
 
     } else {
