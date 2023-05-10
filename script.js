3dc0069369c5435429cd011aedcc645a8aaea0a4
Luciana V Pereira <115096459+lucianavpereira@users.noreply.github.com>
2023-05-10 09:50:37 -0300

    conclusão

--- script.js
+++ script.js
@@ -1,62 +1,21 @@
-// function logar(){
-//     var login = document.getElementById('login').value
-//     var senha = document.getElementById('senha').value
-
-//     if (login == 'admin' && senha == 'admin') {
-//         // alert('sucesso')
-//         // ir para página de cadastro
-//         var url = 'cadastro.html'
-//         var btn = document.querySelector('#botao')
-
-//         function openInNewTab(url) {
-//             var win = window.open(url, '_blank') 
-//             win.focus()
-//         }
-//         btn.addEventListener('click', () => {
-//             openInNewTab(url)
-//         })
-
-
-//     } else {
-//         alert('Acesso negado.')
-//     }
-// }
-
-
-var url = 'cadastro.html'
-var btn = document.querySelector('#botao')
-
-//Com função nomeada----------------
-// function executa(event){
-//     event.preventDefault()
-//     logar(url)
-// }
-
-// btn.addEventListener('click', executa)
-
-//Com função anônima----------------
-// btn.addEventListener('click', function (event){
-//     event.preventDefault()
-//     logar(url)
-// })
-
-//Com arrow function-----------------------
-btn.addEventListener('click', (evento) => {
-    evento.preventDefault()
-    logar(url)
-})
-
-function logar(url){
+function logar(){
     var login = document.getElementById('login').value
     var senha = document.getElementById('senha').value
+
+    var url = 'cadastro.html'
+    var btn = document.querySelector('#botao')
 
     if (login == 'admin' && senha == 'admin') {
         // alert('sucesso')
         // ir para página de cadastro
- 
-        window.open(url,'_self') 
-        // var win = window.open(url,'_blank') 
-        // win.focus()
+        // Com função nomeada----------------
+        
+        btn.addEventListener('click', executa) 
+        function executa(event){
+            event.preventDefault()
+            logar(url)
+        }
+        open(url,'_blank')
 
     } else {
         alert('Acesso negado.')
